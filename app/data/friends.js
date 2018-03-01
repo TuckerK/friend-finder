e75d85bfbb4b019517e1e4c7a34df981d5dc648e
Tucker King <tuckerk23@gmail.com>
2018-03-01 11:29:55 -0700

    some logics

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendsArr = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Jacob Deming",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Louis T. Delia",
+    photo:
+      "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Lou Ritter",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Jordan Biason",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  },
+  {
+    name: "Rick",
+    photo: "https://www.w3schools.com/howto/img_fjords.jpg",
+    scores: ["2", "3", "2", "3", "4", "4", "2", "4", "3", "3"]
+  },
+  {
+    name: "Robert",
+    photo:
+      "https://vignette.wikia.nocookie.net/gameofthrones/images/b/bc/Sandor_Clegane_Season_7_Promo_Image.PNG/revision/latest/scale-to-width-down/343?cb=20170828063713",
+    scores: ["4", "3", "4", "1", "3", "4", "5", "2", "2", "1"]
+  },
+  {
+    name: "Kadiro Elemo",
+    photo:
+      "https://www.facebook.com/alemayehu.kena?hc_ref=ARQx1D8eodCbFnqCxKQIsInx3fDsUAZidTBwkXi-KvLFGU4X4DFkRqAafuJsEZoCjuU&fref=nf",
+    scores: ["5", "2", "2", "3", "4", "1", "3", "5", "2", "3"]
+  },
+  {
+    name: "Kadiro Elemo",
+    photo:
+      "https://www.facebook.com/alemayehu.kena?hc_ref=ARQx1D8eodCbFnqCxKQIsInx3fDsUAZidTBwkXi-KvLFGU4X4DFkRqAafuJsEZoCjuU&fref=nf",
+    scores: ["5", "2", "2", "3", "4", "1", "3", "5", "2", "3"]
+  },
+  {
+    name: "Bob",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/8/83/Spongebob-rainbow-meme-video-16x9.jpg/revision/latest?cb=20170710160900",
+    scores: ["5", "4", "2", "2", "3", "4", "2", "5", "2", "3"]
+  }
+];
+
+module.exports = friendsArr;
