5e0e2e3834793891890f932539f4e6528814d9e4
Tucker King <tuckerk23@gmail.com>
2018-03-01 21:07:06 -0700

    api returning from link and start of match logic

---
+++ app/helpers/findBestMatch.js
@@ -0,0 +1,9 @@
+var match = (usr, friends) =>{
+    friends.forEach(friend => {
+        var scores = friend.scores;
+
+        for (let i = 0; i < scores.length; i++) {
+             
+        }   
+    });
+}
